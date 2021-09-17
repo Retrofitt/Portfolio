5b3cc2e4dfee1bcb1c74871db512d639547f8f52
Retrofitt <rafaelmendozajr94@gmail.com>
2021-09-16 20:15:43 -0700

    style changes

--- src/components/About.js
+++ src/components/About.js
@@ -3,8 +3,7 @@
 export default function About(){
     return(<div className='about-container'>
         <h2>Who are you?</h2>
-        <p>My name is Rafael, friends like to call me 'Raff'.
-            I'm both an aspiring fullstack engineer and an aspiring photographer.
+        <p>My name is Rafael, I'm both an aspiring fullstack engineer and an aspiring photographer.
              At an early age my father helped me put a computer together that would go on to be my first gaming pc back in 2001 and my interest in both hardware and software would only grow over the next 20 years.</p>
         <h2>What are some of your interest/hobbies?</h2>
         <ul>
@@ -14,7 +13,7 @@ export default function About(){
             <li>Skateboarding</li>
             <li>Camping / Outdoor Activities</li>
         </ul>
-        <h2>Front-End Skills</h2>
+        <h2>What are your Front-End Skills?</h2>
         <ul>
             <li>HTML</li>
             <li>JavaScript</li>
@@ -25,7 +24,7 @@ export default function About(){
             </ul>
             <li>CSS</li>
         </ul>
-        <h2>Back-End Skills</h2>
+        <h2>What are your Back-End Skills?</h2>
         <ul>
             <li>n/a</li>
         </ul>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,6 +2,15 @@
 
 export default function Projects(){
     return(<div>
-        <h2>Projects</h2>
+        <h2>Place Holder Projects Header</h2>
+        <div className='project-divs'>
+            <div className='project'><a href='/'>Place-holder<br/>Project 1</a></div>
+            <div className='project'><a href='/'>Place-holder<br/>Project 2</a></div>
+            <div className='project'><a href='/'>Place-holder<br/>Project 3</a></div>
+            <div className='project'><a href='/'>Place-holder<br/>Project 4</a></div>
+            <div className='project'><a href='/'>Place-holder<br/>Project 5</a></div>
+            <div className='project'><a href='/'>Place-holder<br/>Project 6</a></div>
+
+        </div>
     </div>)
 }
