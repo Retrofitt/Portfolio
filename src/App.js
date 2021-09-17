dc284dff3123587fa93d4631bf96883a8ae36c13
Retrofitt <rafaelmendozajr94@gmail.com>
2021-09-16 17:23:49 -0700

    bugfixes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {Route, Link, Switch} from 'react-router-dom'
+import { useParams } from "react-router";
 
 import Home from './components/Home'
 import About from './components/About'
@@ -10,45 +11,47 @@ import Photo from "./components/Photo";
 
 import './styles/styles.css'
 
-// import photo1 from './assests/photos/photo1.jpg'
-// import photo2 from './assests/photos/photo2.jpg'
-// import photo3 from './assests/photos/photo3.jpg'
-// import photo4 from './assests/photos/photo4.jpg'
-// import photo5 from './assests/photos/photo5.jpg'
-// import photo6 from './assests/photos/photo6.jpg'
-// import photo7 from './assests/photos/photo7.jpg'
-// import photo8 from './assests/photos/photo8.jpg'
-// import photo9 from './assests/photos/photo9.jpg'
+import photo1 from './assests/photos/photo1.jpg'
+import photo2 from './assests/photos/photo2.jpg'
+import photo3 from './assests/photos/photo3.jpg'
+import photo4 from './assests/photos/photo4.jpg'
+import photo5 from './assests/photos/photo5.jpg'
+import photo6 from './assests/photos/photo6.jpg'
+import photo7 from './assests/photos/photo7.jpg'
+import photo8 from './assests/photos/photo8.jpg'
+import photo9 from './assests/photos/photo9.jpg'
 import logo1 from './assests/logo/logo1.png'
-// import ig from './assests/logo/ig.png'
-// import linkedin from './assests/logo/linkedin.png'
-// import twitter from './assests/logo/twitter.png'
-// import github from './assests/logo/github.png'
+import ig from './assests/logo/ig.png'
+import linkedin from './assests/logo/linkedin.png'
+import twitter from './assests/logo/twitter.png'
+import github from './assests/logo/github.png'
 
 
 function App() {
 
   const photos = [
-    {image:'https://drive.google.com/thumbnail?id=1rfSRmDB7MqdhqPnqDe7gMa15RrNFKyO6'},
-    {image:'https://drive.google.com/thumbnail?id=1ujpJx6NPW5Ki_etunr1YCVMhht6CHImu'}, 
-    {image:'https://drive.google.com/thumbnail?id=1hQsLAV5U44N7qmpkfNXoI8fJ-OZsKyap'}, 
-    {image:'https://drive.google.com/thumbnail?id=1KLj34EasGdDThg889WwN3hK3OKlmlewJ'}, 
-    {image:'https://drive.google.com/thumbnail?id=1XN9kP0uU4wqIJ1ESi98WOk7ZONmBU-BN'}, 
-    {image:'https://drive.google.com/thumbnail?id=1nu3fvR6G30zAu1J690yf4y0Prdb8SY5P'}, 
-    {image:'https://drive.google.com/thumbnail?id=181N36SA1HoFU8JLRQUw4MCe1KCJvE0qW'}, 
-    {image:'https://drive.google.com/thumbnail?id=1tpzoHsJyAFzMxe2ob1UMCk1yxAKfjuOz'}, 
-    {image:'https://drive.google.com/thumbnail?id=1Bf8pP0G7P2SdBWE7uB3M00kxq5uwD0_W'}
+    {image:photo1},
+    {image:photo2}, 
+    {image:photo3}, 
+    {image:photo4}, 
+    {image:photo5}, 
+    {image:photo6}, 
+    {image:photo7}, 
+    {image:photo8}, 
+    {image:photo9}
   ]
 
 
   const socials = {
-    ig:'https://drive.google.com/thumbnail?id=1jxtAR-KhKBEL2xgN7IMiFjwObcVw7Cvm', 
-    linkedin:'https://drive.google.com/thumbnail?id=1dqQW75Lx5Zmlbp5TaaOYbE9wy8BITFNN', 
-    twitter:'https://drive.google.com/thumbnail?id=1DiCf56nqOSf8h_36DprDL1WW69oaX_I5', 
-    github:'https://drive.google.com/thumbnail?id=1jeg6aoYx8dy8fTojgGcT2Xn0RiKYklrl'
+    ig:ig, 
+    linkedin:linkedin, 
+    twitter:twitter, 
+    github:github
   }
 
 
+
+  const id = useParams()
   return (<div className="App">
 
     <nav className='nav-bar'>
