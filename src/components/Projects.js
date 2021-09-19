6fae632320585aab18300a1c78e270ba4620d590
Retrofitt <rafaelmendozajr94@gmail.com>
2021-09-18 18:16:16 -0700

    style changes, added project one

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,14 +2,13 @@
 
 export default function Projects(){
     return(<div>
-        <h2>Place Holder Projects Header</h2>
         <div className='project-divs'>
-            <a href='https://retrosghbusinesscard.vercel.app/'><div className='project'>Github<br/>Digital Business Card</div></a>
-            <a href='/'><div className='project'>Place-holder<br/>Project 2</div></a>
-            <a href='/'><div className='project'>Place-holder<br/>Project 3</div></a>
-            <a href='/'><div className='project'>Place-holder<br/>Project 4</div></a>
-            <a href='/'><div className='project'>Place-holder<br/>Project 5</div></a>
-            <a href='/'><div className='project'>Place-holder<br/>Project 6</div></a>
+            <a href='https://retrosghbusinesscard.vercel.app/'><div className='project one'>Github<br/>Digital Business Card</div></a>
+            <a href='/'><div className='project two'>Place-holder<br/>Project 2</div></a>
+            <a href='/'><div className='project three'>Place-holder<br/>Project 3</div></a>
+            <a href='/'><div className='project four'>Place-holder<br/>Project 4</div></a>
+            <a href='/'><div className='project five'>Place-holder<br/>Project 5</div></a>
+            <a href='/'><div className='project six'>Place-holder<br/>Project 6</div></a>
 
         </div>
     </div>)
