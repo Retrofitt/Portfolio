4d5df339ee3651207a0be8f7094e43b4cfa63007
Retrofitt <rafaelmendozajr94@gmail.com>
2021-12-24 18:05:22 -0800

    Minor Style Fixes (cleaning up CSS)

--- src/components/Photography.js
+++ src/components/Photography.js
@@ -8,9 +8,9 @@ export default function Photography(props) {
     <div className="photo-container">
       <div className="photo-container-header"></div>
       <div className="photo-content">
-        {photos.map((photo, i) => {
+        {/* {photos.map((photo, i) => {
           return (
-            <div key={i} className="photos">
+            <div key={i} >
               <div>
                 <Image.PreviewGroup get>
                   <Image width={200} src={photo.image} />
@@ -18,7 +18,17 @@ export default function Photography(props) {
               </div>
             </div>
           );
-        })}
+        })} */}
+        <Image.PreviewGroup>
+          <Image width={200} src={photos[0].image} />
+          <Image width={200} src={photos[1].image} />
+          <Image width={200} src={photos[2].image} />
+          <Image width={200} src={photos[3].image} />
+          <Image width={200} src={photos[4].image} />
+          <Image width={200} src={photos[5].image} />
+          <Image width={200} src={photos[6].image} />
+          <Image width={200} src={photos[7].image} />
+        </Image.PreviewGroup>
       </div>
     </div>
   );
